e562498325f7109af7714e68764bd9191f5ba83c
YushfaSaleem1 <babydoodles66@gmail.com>
2025-01-21 01:25:56 +0500

    Incorporated JavaScript to display dynamic alerts, providing visitors with real-time notifications
    Created a dynamic and interactive landing page using HTML5, CSS3, and JavaScript.

---
+++ main.js
@@ -0,0 +1,10 @@
+function showpopup() {
+    alert("Congratulations! You're about to enjoy a free upgrade for one month. Click 'OK' to proceed and unlock premium features!");
+};
+
+
+function popup() {
+    alert("Your free trial is about to start! Enjoy full access to premium features for 7 days. No charges during the trial period")
+;
+
+}
